5ea36d6ca1706e21202d72386d81d5d688206eb3
KrazyKrok <69294123+krazykrok@users.noreply.github.com>
2020-08-06 16:24:46 +1000

    Add files via upload
    Initial Commit

---
+++ shoot_the_fruit.py
@@ -0,0 +1,35 @@
+import pygame
+pygame.display.init()
+screen = pygame.display.set_mode((800, 600))
+from pgzero.builtins import Actor
+from random import randint
+apple = Actor("apple")
+score = 0
+WIDTH = 800
+HEIGHT = 600
+
+def draw():
+    screen.clear()
+    apple.draw()
+    
+def place_apple():
+    apple.x = randint(10, 800)
+    apple.y = randint(10, 600)
+    
+def on_mouse_down(pos):
+    global score
+    if apple.collidepoint(pos):
+        print("Good Shot!")
+        place_apple()
+        score = score + 1
+        display_score()
+    else:
+        print("You Missed!")
+        quit()        
+        
+def display_score():
+    print(score)
+    
+        
+
+place_apple()
